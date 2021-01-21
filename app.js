775f3f3ee2e133f7bb9898707e4646ca634050d2
“Debora <debora.crescenzo@gmail.com>
2021-01-21 10:10:15 +0000

    test

--- app.js
+++ app.js
@@ -3,34 +3,43 @@ const app = Vue.createApp({
     return {
       showBooks: true,
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
-        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
       ],
-    }
+    };
   },
   methods: {
     toggleShowBooks() {
-      this.showBooks =  !this.showBooks
+      this.showBooks = !this.showBooks;
     },
     toggleFav(book) {
       book.isFav = !book.isFav;
-      
-      // if (book.isFav) {
-      //   book.isFav = false;
-      // } else {
-      //   book.isFav = true;
-      // }
     },
   },
   computed: {
     filteredBooks() {
-      return this.books.filter(book => book.isFav)
-    }
-  }
-})
+      return this.books.filter((book) => book.isFav);
+    },
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
 
 // Challenge - Add to Favs
 //   - attach a click event to each li tag (for each book)
